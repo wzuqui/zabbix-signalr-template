af9d781c1390bd7ca08a456b69eabb3df0575da2
wzuqui <willian.e.marcy@gmail.com>
2019-09-03 19:23:02 -0300

    Update signalr_counters.ps1

--- signalr_counters.ps1
+++ signalr_counters.ps1
@@ -1,20 +1,21 @@
-write-host "{"
-write-host " `"data`":[`n"
-foreach ($name in Get-Counter -ListSet * | Where-Object CounterSetName -eq "SignalR" | Select -ExpandProperty PathsWithInstances)
+clear
+
+$lista = Get-Counter -ListSet * | Where-Object CounterSetName -eq "SignalR" | Select -ExpandProperty PathsWithInstances
+
+
+$list = New-Object System.Collections.ArrayList;
+foreach($name in Get-Counter -ListSet * | Where-Object CounterSetName -eq "SignalR" | Select -ExpandProperty PathsWithInstances)
 {
-    if ($idx -lt $basename.Rows.Count)
-        {
-            $line= "{ `"{#SIGNALRNAME}`" : `"" + $name + "`" }," | convertto-encoding "cp866" "utf-8"
-            write-host $line
-        }
-    elseif ($idx -ge $basename.Rows.Count)
-        {
-            $line= "{ `"{#SIGNALRNAME}`" : `"" + $name + "`" }" | convertto-encoding "cp866" "utf-8"
-            write-host $line
-        }
-    $idx++;
+    [regex]$regex = '.+\(([\w-]+)\)\\(.+)';
+    
+    $item = New-Object System.Object
+    $item | Add-Member -MemberType NoteProperty -Name "#{SIGNALAPPNAME}" -Value $regex.Matches($name).groups[1].Value
+    $item | Add-Member -MemberType NoteProperty -Name "#{SIGNALCOUNTERNAME}" -Value $regex.Matches($name).groups[2].Value
+
+    $list.Add($item) | Out-Null;
 }
 
- write-host
-write-host " ]"
+write-host "{"
+write-host " `"data`":"
+$list | ConvertTo-Json
 write-host "}"
