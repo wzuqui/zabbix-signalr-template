9050d90a8f1968e91ce3f682039586edcc92349d
wzuqui <willian.e.marcy@gmail.com>
2019-09-03 19:43:54 -0300

    Update signalr_counters.ps1

--- signalr_counters.ps1
+++ signalr_counters.ps1
@@ -1,21 +1,34 @@
 clear
 
-$lista = Get-Counter -ListSet * | Where-Object CounterSetName -eq "SignalR" | Select -ExpandProperty PathsWithInstances
-
-
 $list = New-Object System.Collections.ArrayList;
 foreach($name in Get-Counter -ListSet * | Where-Object CounterSetName -eq "SignalR" | Select -ExpandProperty PathsWithInstances)
 {
     [regex]$regex = '.+\(([\w-]+)\)\\(.+)';
     
     $item = New-Object System.Object
-    $item | Add-Member -MemberType NoteProperty -Name "#{SIGNALAPPNAME}" -Value $regex.Matches($name).groups[1].Value
-    $item | Add-Member -MemberType NoteProperty -Name "#{SIGNALCOUNTERNAME}" -Value $regex.Matches($name).groups[2].Value
+    $item | Add-Member -MemberType NoteProperty -Name name -Value $regex.Matches($name).groups[1].Value
+    $item | Add-Member -MemberType NoteProperty -Name proc -Value $regex.Matches($name).groups[2].Value
 
     $list.Add($item) | Out-Null;
 }
-
+$idx = 1
 write-host "{"
-write-host " `"data`":"
-$list | ConvertTo-Json
+write-host " `"data`":[`n"
+foreach ($name in $list)
+{
+    if ($idx -lt $list.Count)
+        {
+            $line= "{ `"{#SIGNALAPPNAME}`" : `"" + $name.name + "`, `"{#SIGNALCOUNTERNAME}`" : `"" + $name.proc + "`" }," | convertto-encoding "cp866" "utf-8"
+            write-host $line
+        }
+    elseif ($idx -ge $list.Rows.Count)
+        {
+            $line= "{ `"{#SIGNALAPPNAME}`" : `"" + $name.name + "`, `"{#SIGNALCOUNTERNAME}`" : `"" + $name.proc + "`" }" | convertto-encoding "cp866" "utf-8"
+            write-host $line
+        }
+    $idx++;
+}
+
+ write-host
+write-host " ]"
 write-host "}"
